5273cb6583d319e307a2377c2494d82df3258f0c
Andrea <baraldian@gmail.com>
2024-04-12 16:51:53 +0200

    add: init imports

--- src/fairnesseval/__init__.py
+++ src/fairnesseval/__init__.py
@@ -1,6 +1,10 @@
+import importlib
 from os.path import dirname, basename, isfile, join
 import glob
 modules = glob.glob(join(dirname(__file__), "*.py"))
 __all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
+
+for module in __all__:
+    importlib.import_module('.' + module, package='fairnesseval')
 
 __version__ = '0.1.0'
