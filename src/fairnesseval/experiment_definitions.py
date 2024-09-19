c66b0b5db61504ef3e3c7f030dc75308f48e749f
Andrea <baraldian@gmail.com>
2024-09-19 18:11:42 +0200

    add: demo experiment with datasets without Sensitive attribute; specify param grid for base model.

--- src/fairnesseval/experiment_definitions.py
+++ src/fairnesseval/experiment_definitions.py
@@ -27,7 +27,7 @@ experiment_definitions = [
     },
     {
         'experiment_id': 'demo.1.test',
-        'dataset_names': ['adult_sigmod', 'compas'],
+        'dataset_names': ['adult_sigmod_no_SA', 'compas_no_SA'],
         'model_names': ['fairlearn'],
         'random_seeds': [1],
         'model_params': {'eps': [0.005, 0.15],
@@ -35,7 +35,17 @@ experiment_definitions = [
                          'base_model_code': 'lr',
                          'eta0': [2.0],
                          'run_linprog_step': [False],
-                         'max_iter': [25, 50]},
+                         'max_iter': [25, 50],
+                         'base_model_grid_params': dict(
+                             solver=[  # 'newton-cg', 'liblinear',
+                                 'lbfgs'],
+                             penalty=['l1', 'l2'
+                                      ],
+                             C=[0.01,  1, 1000],
+                             # [10, 1.0, 0.1, 0.05, 0.01],
+                             max_iter=[200],
+                         )
+                         },
         'results_path': 'demo_results',
         'train_test_fold': [0],
         'params': ['--debug']
